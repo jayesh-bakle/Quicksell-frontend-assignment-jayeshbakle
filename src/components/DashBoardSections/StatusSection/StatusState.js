4adfab43e151e89ee580956b19bf5f3b3faae5cc
jayesh-bakle <jayeshbakleabd@gmail.com>
2024-10-09 22:52:10 +0530

    updated status states

---
+++ src/components/DashBoardSections/StatusSection/StatusState.js
@@ -0,0 +1,23 @@
+import Done from "../../../images/Done.svg"
+import Cancelled from "../../../images/Cancelled.svg"
+import Todo from "../../../images/To-do.svg"
+import Backlog from "../../../images/Backlog.svg"
+import InProgress from "../../../images/in-progress.svg"
+import './StatusState.css'
+const StatusState=({status})=>{
+    const statusIcons = {
+        "Done": <img src={Done} alt="Done" />,
+        "Cancelled": <img src={Cancelled} alt="Cancelled" />,
+        "Todo": <img src={Todo} alt="Todo" />,
+        "Backlog": <img src={Backlog} alt="Backlog" />,
+        "In progress": <img src={InProgress} alt="Your SVG" />,
+      };
+    return (
+        <>
+        <span className="statusIcon">
+        {statusIcons[status]}
+        </span>
+        </>
+    )
+}
+export default StatusState;
